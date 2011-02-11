0eb4acf0375b7259da16c6b21ce433b8e0b7b553
ilsoluin <ilsoluin@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-02-11 03:03:39 +0000

    updated. Login works. pw is admin

--- prototype/admin_page.php
+++ prototype/admin_page.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Admin Panel</title>
-</head>
-
-<body>
 <p>Administrator's Panel</p>
 <p>Upload Experiment</p>
-<p>
+
   <input type="file" name="upload_file"/>
-</p>
-<p>
-  <input type="submit" name="upload_exp" id="uploadbutton" value="Upload" />
-</p>
+  
+  <input type="submit" 
+  name="upload_exp" 
+  id="uploadbutton" 
+  value="Upload"
+  onclick="uploadExperiment()" />
+  
 <p>&nbsp;</p>
 <table width="400" border="1">
   <caption>
@@ -29,10 +24,3 @@
   </tr>
 </table>
 <p>&nbsp;</p>
-
-<?php
-
-?>
-
-</body>
-</html>
---
+++ prototype/login_confirmation.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login Confirmation</title>
+</head>
+
+<body>
+
+<?php
+
+	$password = $_POST["password"];
+	
+	if($password === 'admin') {
+		echo 'Login Successful';
+		//log in and re-direct to admin panel.
+		include("admin_page.php");
+		
+	}
+	else {
+		echo 'Login Failed';
+	}
+	
+?>
+
+
+</body>
+</html>
