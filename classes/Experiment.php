4a7387b2d5f85c0e81ce51e32fdfb0ef14fbe431
brotcke <brotcke@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-02-27 06:41:59 +0000

    updated Experiment class, Survey class, and Excel file

--- classes/Experiment.php
+++ classes/Experiment.php
@@ -1,35 +1,25 @@
 <?php
 include '../includes/PHPExcel/IOFactory.php';
+include '../classes/Survey.php';
 
-class ExperimentReader {
+class Experiment {
 	
-	public $experimentProperties = array();
-	public $experimentProperties = array();
-	public $experimentItemCodes = array();
-	public $experimentItems = array();
-	public $experimentResponseTypes = array();
+	public $surveys = array();
 	
-	public function loadExperiment($filename) {
+	public function loadExperimentFromExcelFile($filename) {
 		
 		if (!file_exists("../experiments/".$filename)) {
 			exit("Error: File ".$filename." does not exist.");
 		}
 		
 		$objPHPExcel = PHPExcel_IOFactory::load("../experiments/".$filename);
-		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 		
-		//echo "Read from cell B7: ",$objPHPExcel->getActiveSheet()->getCell('B7')->getValue();
-		
-		$experimentProperties["questionsPerPage"] = $objPHPExcel->getActiveSheet()->getCell('B7')->getValue();
-		
-		$count = 55;
-		$question = 0;
-		while ($objPHPExcel->getActiveSheet()->getCell("A".$count)->getValue() != "") {
-			$experimentItemCodes[$question] = $objPHPExcel->getActiveSheet()->getCell("A".$count)->getValue();
-			$experimentItems[$question] = $objPHPExcel->getActiveSheet()->getCell("B".$count)->getValue();
-			$experimentResponseTypes[$question] = $objPHPExcel->getActiveSheet()->getCell("C".$count)->getValue();
+		$count = 0;
+		foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+			$survey = new Survey();
+			$survey->loadWorksheet($worksheet);
+			$surveys[$count] = $survey;
 			$count++;
-			$question++;
 		}
 		
 		//var_dump(get_defined_vars());
