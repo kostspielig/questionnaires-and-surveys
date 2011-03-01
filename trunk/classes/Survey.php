ce0177bf93e6827707d24e91f29316cc9dfd516d
brotcke <brotcke@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-03-01 04:38:17 +0000

    fixed Survey and Experiment class

--- trunk/classes/Survey.php
+++ trunk/classes/Survey.php
@@ -14,10 +14,59 @@ class Survey {
 	
 	public function loadWorksheet($worksheet) {
 		
-		$surveyProperties["questionsPerPage"] = $worksheet->getCell('B7')->getValue();
-		$surveyProperties["name"] = $worksheet->getTitle();		
+		$surveyProperties["name"] = $worksheet->getTitle();
+		$surveyProperties["outputFilename"] = $worksheet->getCell('B2')->getValue();
+		$surveyProperties["cssFilename"] = $worksheet->getCell('B3')->getValue();
+		$surveyProperties["thankYouPage"] = $worksheet->getCell('B4')->getValue();
+
+		$surveyProperties["surveyTableProperties_questionsPerPage"] = $worksheet->getCell('B7')->getValue();
+		$surveyProperties["surveyTableProperties_width"] = $worksheet->getCell('B8')->getValue();
+		$surveyProperties["surveyTableProperties_alignment"] = $worksheet->getCell('B9')->getValue();
+		$surveyProperties["surveyTableProperties_borderThickness"] = $worksheet->getCell('B10')->getValue();
+		$surveyProperties["surveyTableProperties_cellPadding"] = $worksheet->getCell('B11')->getValue();
+		$surveyProperties["surveyTableProperties_cellSpacing"] = $worksheet->getCell('B12')->getValue();
+
+		$surveyProperties["headerProperties_customHeader"] = $worksheet->getCell('B15')->getValue();
+		$surveyProperties["headerProperties_leftTitle"] = $worksheet->getCell('B16')->getValue();
+		$surveyProperties["headerProperties_rightTitle"] = $worksheet->getCell('B17')->getValue();
+		$surveyProperties["headerProperties_alignment"] = $worksheet->getCell('B18')->getValue();
+		$surveyProperties["headerProperties_borderThickness"] = $worksheet->getCell('B19')->getValue();
+		$surveyProperties["headerProperties_padding"] = $worksheet->getCell('B20')->getValue();
+		$surveyProperties["headerProperties_spacing"] = $worksheet->getCell('B21')->getValue();
+
+		$surveyProperties["surveyLeftColumnProperties_width"] = $worksheet->getCell('B24')->getValue();
+		$surveyProperties["surveyLeftColumnProperties_alignment"] = $worksheet->getCell('B25')->getValue();
+		$surveyProperties["surveyLeftColumnProperties_borderThickness"] = $worksheet->getCell('B26')->getValue();
+		$surveyProperties["surveyLeftColumnProperties_padding"] = $worksheet->getCell('B27')->getValue();
+		$surveyProperties["surveyLeftColumnProperties_spacing"] = $worksheet->getCell('B28')->getValue();
 		
-		$count = 55;
+		$surveyProperties["surveyRightColumnProperties_width"] = $worksheet->getCell('B31')->getValue();
+		$surveyProperties["surveyRightColumnProperties_alignment"] = $worksheet->getCell('B32')->getValue();
+		$surveyProperties["surveyRightColumnProperties_borderThickness"] = $worksheet->getCell('B33')->getValue();
+		$surveyProperties["surveyRightColumnProperties_padding"] = $worksheet->getCell('B34')->getValue();
+		$surveyProperties["surveyRightColumnProperties_spacing"] = $worksheet->getCell('B35')->getValue();
+		
+		$surveyProperties["paginationTextProperties_view"] = $worksheet->getCell('B38')->getValue();
+		$surveyProperties["paginationTextProperties_alignment"] = $worksheet->getCell('B39')->getValue();
+		$surveyProperties["paginationTextProperties_position"] = $worksheet->getCell('B40')->getValue();
+		
+		$surveyProperties["paginationButtonsTableProperties_width"] = $worksheet->getCell('B43')->getValue();
+		$surveyProperties["paginationButtonsTableProperties_alignment"] = $worksheet->getCell('B44')->getValue();
+		$surveyProperties["paginationButtonsTableProperties_borderThickness"] = $worksheet->getCell('B45')->getValue();
+		$surveyProperties["paginationButtonsTableProperties_padding"] = $worksheet->getCell('B46')->getValue();
+		$surveyProperties["paginationButtonsTableProperties_spacing"] = $worksheet->getCell('B47')->getValue();
+		
+		$count = 50;
+		$question = 0;
+		$cellValue = $worksheet->getCell('A'.$count)->getValue();
+		while ($cellValue != "") {
+			$surveyUserInfo[$question] = $worksheet->getCell('A'.$count)->getValue();
+			$count++;
+			$question++;
+			$cellValue = $worksheet->getCell('A'.$count)->getValue();
+		}
+		
+		$count += 2;
 		$question = 0;
 		while ($worksheet->getCell("A".$count)->getValue() != "") {
 			$surveyItemCodes[$question] = $worksheet->getCell("A".$count)->getValue();
@@ -27,7 +76,7 @@ class Survey {
 			$question++;
 		}
 		
-		//var_dump(get_defined_vars());
+		var_dump(get_defined_vars());
 	}
 }
 
