e44ecb9085b107ec33716df3d663f94d4db7ed17
kostspielig@gmail.com <kostspielig@gmail.com@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-03-02 03:34:27 +0000

    change design doc change database

--- trunk/classes/Model.php
+++ trunk/classes/Model.php
@@ -24,13 +24,31 @@ class Model {
 		return $res;
 	}
 	
-	public function uploadExperiment(Experiment $exp, $name, $admin) {
+	public function uploadExperiment(Experiment $exp, $name, $filename, $admin) {
 		$this->db->open();
 		// Insert experiment
-		$exp_id = $this->db->insertExperiment("NULL, '".$name."', '".$admin."'");
+		$exp_id = $this->db->insertExperiment("NULL, '".$name."', '".$filename."','".$admin."'");
 		// Insert survey
 		foreach ($exp->$surveys as $sur) {
-			$sur_id = $this->db->insertSurvey("NULL,'".$exp_id."', '".$sur->surveyProperties['name']."'");
+			$sur_id = $this->db->insertSurvey("NULL,'".$exp_id."', '".$sur->surveyProperties['name']."', 
+			'".$sur->surveyProperties['surveyTableProperties_pseudoRandomWidth']."', '".$sur->surveyProperties['outputFilename']."', 
+			'".$sur->surveyProperties['cssFilename']."', '".$sur->surveyProperties['thankYouPage']."', 
+			'".$sur->surveyProperties['surveyTableProperties_questionsPerPage']."', '".$sur->surveyProperties['surveyTableProperties_width']."', 
+			'".$sur->surveyProperties['surveyTableProperties_alignment']."', '".$sur->surveyProperties['surveyTableProperties_borderThickness']."', 
+			'".$sur->surveyProperties['surveyTableProperties_cellPadding']."', '".$sur->surveyProperties['surveyTableProperties_cellSpacing']."', 
+			'".$sur->surveyProperties['headerProperties_customHeader']."', '".$sur->surveyProperties['headerProperties_leftTitle']."',
+			'".$sur->surveyProperties['headerProperties_rightTitle']."', '".$sur->surveyProperties['headerProperties_alignment']."',
+			'".$sur->surveyProperties['headerProperties_borderThickness']."', '".$sur->surveyProperties['headerProperties_padding']."',
+			'".$sur->surveyProperties['headerProperties_spacing']."', '".$sur->surveyProperties['surveyLeftColumnProperties_width']."',
+			'".$sur->surveyProperties['surveyLeftColumnProperties_alignment']."', '".$sur->surveyProperties['surveyLeftColumnProperties_borderThickness']."',
+			'".$sur->surveyProperties['surveyLeftColumnProperties_padding']."', '".$sur->surveyProperties['surveyLeftColumnProperties_spacing']."',
+			'".$sur->surveyProperties['surveyRightColumnProperties_width']."', '".$sur->surveyProperties['surveyRightColumnProperties_alignment']."',
+			'".$sur->surveyProperties['surveyRightColumnProperties_borderThickness']."', '".$sur->surveyProperties['surveyRightColumnProperties_padding']."',
+			'".$sur->surveyProperties['surveyRightColumnProperties_spacing']."', '".$sur->surveyProperties['paginationTextProperties_view']."',
+			'".$sur->surveyProperties['paginationTextProperties_alignment']."', '".$sur->surveyProperties['paginationTextProperties_position']."',
+			'".$sur->surveyProperties['paginationButtonsTableProperties_width']."', '".$sur->surveyProperties['paginationButtonsTableProperties_alignment']."',
+			'".$sur->surveyProperties['paginationButtonsTableProperties_borderThickness']."', '".$sur->surveyProperties['paginationButtonsTableProperties_padding']."',
+			'".$sur->surveyProperties['paginationButtonsTableProperties_spacing']."'");
 			/*WE NEED MANY MORE FIELDS! */
 			
 			// Insert User Info Questions
