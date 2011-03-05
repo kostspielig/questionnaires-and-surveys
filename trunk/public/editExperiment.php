f951c0de354b89af8b630c47aa454c3197706cf5
brotcke <brotcke@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-03-05 04:54:07 +0000

    fixed small typo for filename

--- trunk/public/editExperiment.php
+++ trunk/public/editExperiment.php
@@ -112,7 +112,7 @@ $download_path = "../experiments/";
 
 
 // Combine the download path and the filename to create the full path to the file.
-$file = "$download_path$file";
+$file = "$download_path$filename";
 
 set_time_limit(0);
 output_file($file, $filename, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
