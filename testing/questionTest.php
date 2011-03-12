92d935015e229771a6d22f7d0567f083192b9d92
brotcke <brotcke@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-03-12 23:54:42 +0000

    updated Question class, realized there are different types of questions

--- testing/questionTest.php
+++ testing/questionTest.php
@@ -4,7 +4,16 @@ require_once '../includes/classes.php';
 $question = new Question();
 echo '<br/>',$question->toString();
 
-$question = new Question("itemCode123", "item12312", "responseType1212");
+$question = new UserQuestion();
+echo '<br/>',$question->toString();
+
+$question = new UserQuestion("arg1", "arg2");
+echo '<br/>',$question->toString();
+
+$question = new SurveyQuestion();
+echo '<br/>',$question->toString();
+
+$question = new SurveyQuestion("arg1", "arg2", "arg3");
 echo '<br/>',$question->toString();
 
 ?>
