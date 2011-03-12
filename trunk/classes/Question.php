dc2d2820050cdf0a322d26b91be90196ad468a5d
brotcke <brotcke@cfc7dd54-cf2d-ca87-1650-35cf32175705>
2011-03-12 23:54:42 +0000

    updated Question class, realized there are different types of questions

--- trunk/classes/Question.php
+++ trunk/classes/Question.php
@@ -1,31 +1,53 @@
 <?php
 class Question {
-	public $itemCode;
-	public $item;
-	public $responseType;
-	public $response;
-	//public $date;
-	
-	public function Question() {
-		// Allows for overloaded constructors
-		$a = func_get_args(); 
-        $i = func_num_args(); 
-        if (method_exists($this,$f='Question'.$i)) { 
-            call_user_func_array(array($this,$f),$a); 
-        }
-	}
-	
-	function Question3($a1,$a2,$a3) 
-    { 
-        $this->itemCode = $a1;
-		$this->item = $a2;
-		$this->responseType = $a3;
-    }
-    
 	public function toString()
     {
     	var_dump($this);
     }
-	
+}
 
+// This class is for reguler survey questions
+class SurveyQuestion extends Question {
+	public $itemCode;
+	public $item;
+	public $responseType;
+	public $response;
+	//TODO: public $date;
+	
+	public function SurveyQuestion() {
+		// Allows for overloaded constructors
+		$a = func_get_args(); 
+        $i = func_num_args(); 
+        if (method_exists($this,$f='SurveyQuestion'.$i)) { 
+            call_user_func_array(array($this,$f),$a); 
+        }
+	}
+	
+	function SurveyQuestion3($itemCode, $item, $responseType) 
+    { 
+        $this->itemCode = $itemCode;
+		$this->item = $item;
+		$this->responseType = $responseType;
+    }
+}
+
+// This class is for questions pertaining to user info such as age, sex, etc.
+class UserQuestion extends Question {
+	public $userItem;
+	public $userResponse;
+	
+	public function UserQuestion() {
+		// Allows for overloaded constructors
+		$a = func_get_args(); 
+        $i = func_num_args(); 
+        if (method_exists($this,$f='UserQuestion'.$i)) { 
+            call_user_func_array(array($this,$f),$a); 
+        }
+	}
+	
+	function UserQuestion2($userItem, $userResponse) 
+    { 
+        $this->userItem = $userItem;
+        $this->userResponse = $userResponse;
+    }  	
 }
