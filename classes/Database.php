e793f18bc0e144bd6d8f1ab9e8d88fb2dd2fac9a
kostspielig <kostspielig@gmail.com>
2011-03-14 10:46:45 +0000

    Download results looking pretty good!!!

--- classes/Database.php
+++ classes/Database.php
@@ -256,11 +256,11 @@ class Database
 	}
 	
 	public function getItemCode($que_id) {
-		$res = sqlite_query($this->dbhandle, "SELECT item_code FROM question WHERE que_id = $que_id");
+		$result = sqlite_query($this->dbhandle, "SELECT * FROM question WHERE que_id = $que_id");
 		if (!$result) die("Cannot execute query.");
 		$row = sqlite_fetch_object($result);
 		
-		return ($row!= null)?$res:0;
+		return ($row!= null)?$row->item_code:0;
 	}
 	
 	public function extractAnswersToCVS($exp_id, $name) {
@@ -291,10 +291,10 @@ class Database
 				}*/
 				
 				//print answers to questions
-				$res4 = sqlite_query($this->dbhandle, "SELECT * FROM answer WHERE part_id = $par->part_id ORDER BY positions DESC");
+				$res4 = sqlite_query($this->dbhandle, "SELECT * FROM answer WHERE part_id = $par->part_id ORDER BY position DESC");
 				if (!$res4) die("Cannot execute query.");
 				while($ans = sqlite_fetch_object($res4)) {
-					$item_code = getItemCode($ans->que_id);
+					$item_code = $this->getItemCode($ans->que_id);
 					$list[] = array($i, $ans->date, $ans->position, $item_code, $ans->answer);
 				}
 				$i ++;
